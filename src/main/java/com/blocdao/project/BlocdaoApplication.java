563bab4b01fca4833f0145b31e6f1a48d9cf3834
whwjdan <whwjdan@naver.com>
2022-08-19 16:30:16 +0900

    initialize project

---
+++ src/main/java/com/blocdao/project/BlocdaoApplication.java
@@ -0,0 +1,13 @@
+package com.blocdao.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlocdaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlocdaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blocdao/project/BlocdaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blocdao.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlocdaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

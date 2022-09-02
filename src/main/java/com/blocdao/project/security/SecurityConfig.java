1bdbff107adde053e0846fafa8778d1433012c84
whwjdan <whwjdan@naver.com>
2022-09-02 20:14:33 +0900

    feat: @EnableWebSecurity 주석 및 SecurityConfig 수정

--- src/main/java/com/blocdao/project/security/SecurityConfig.java
+++ src/main/java/com/blocdao/project/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.blocdao.project.security;
 
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,7 +21,6 @@ public class SecurityConfig {
         http
                 .httpBasic().disable()
                 .csrf().disable()
-                .formLogin().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
@@ -35,7 +36,7 @@ public class SecurityConfig {
     public WebSecurityCustomizer configure() {
         return web -> web.ignoring()
                 .antMatchers(HttpMethod.GET ,"/")
-                .antMatchers(HttpMethod.POST, "/users")
+                .antMatchers(HttpMethod.POST, "/member")
                 .antMatchers("/css/**")
                 .antMatchers("/static/**")
                 .antMatchers("/js/**")
@@ -44,6 +45,9 @@ public class SecurityConfig {
                 .antMatchers("/vendor/**")
                 .antMatchers("/favicon.ico")
                 .antMatchers("/pages/**")
+                .antMatchers("/h2-console/**")
+                .antMatchers("/api/**")
+                .antMatchers("/swagger-ui/**")
                 .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**");
     }
 }

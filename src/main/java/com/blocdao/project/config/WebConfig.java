783c1afbbe2d34343726b3f81354b121e6001716
godongs <asd236678@gmail.com>
2022-09-05 10:09:45 +0900

    add EnableWebMvc

--- src/main/java/com/blocdao/project/config/WebConfig.java
+++ src/main/java/com/blocdao/project/config/WebConfig.java
@@ -1,10 +1,14 @@
 package com.blocdao.project.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ComponentScan
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override

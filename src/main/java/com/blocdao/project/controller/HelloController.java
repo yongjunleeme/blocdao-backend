6531fda91b6181b6d44631175c35c26b8c3d862d
cloudwi <cloudwi@naver.com>
2022-09-05 12:36:44 +0900

    member test 작성 완료

---
+++ src/main/java/com/blocdao/project/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.blocdao.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello";
+    }
+}
+

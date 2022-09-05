f4782215d5ce6a7d18f709fbbab227d6be0bbc3e
cloudwi <cloudwi@naver.com>
2022-09-05 11:48:39 +0900

    member test 작성 완료

--- src/main/java/com/blocdao/project/security/ProdSecurityConfig.java
+++ src/main/java/com/blocdao/project/security/ProdSecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Profile("prod")
+@Configuration
+@EnableWebSecurity
 //@Configuration
 //@EnableWebSecurity
 public class ProdSecurityConfig extends SecurityConfig {
